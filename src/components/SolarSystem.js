917c644e2561f12c77ce8fbf5a03cec3de9e49ed
Victor Murilo <victor2706.vm@gmail.com>
2022-06-21 15:26:32 -0300

    Cumpri o requisito 06

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {
+          planets.map((planeta) => {
+            const { name, image } = planeta;
+            return <PlanetCard planetName={ name } planetImage={ image } key={ name } />;
+          })
+        }
       </div>
     );
   }
